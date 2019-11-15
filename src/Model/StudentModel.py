ff2ae67086bffe3d7564bf75b10a337c720b7eac
LevSergey <57245714+LevSergey@users.noreply.github.com>
2019-11-15 09:36:51 +0400

    Update StudentModel.py

--- src/Model/StudentModel.py
+++ src/Model/StudentModel.py
@@ -1,13 +1,19 @@
-StudentModel(object):
-def __init__(self)
-self.surname = None
-self.nale = None
-self.family = None
-def getName(self):
-return self.name 
-def setName(self,name):
-self.name = name
-self.StudentModel = StudentModel()
+class StudentModel(object):
+  def __init__(self):
+    self.surname = None
+    self.nale = None
+    self.family = None
+  def getName(self):
+    return self.name 
+  def setName(self,name):
+    self.name = name
+    self.StudentModel = StudentModel()
 nameLE = QLineEdit()
-def setName(self,text):
-self.StudentModel.name = text
+  def setName(self,text):
+    self.StudentModel.name = text
+  def.accept(self):
+    self.table.addData()
+    self.close()
+    self.StudentModel = StudentModel()
+    nameLE = QLineEdit()
+    nameLE.textChange.connect(set Name)
