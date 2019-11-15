94343f157c756aaf88e5887227937814a26ec2b9
LevSergey <57245714+LevSergey@users.noreply.github.com>
2019-11-15 09:02:13 +0400

    Create StudentModel.py

---
+++ src/Model/StudentModel.py
@@ -0,0 +1,13 @@
+StudentModel(object):
+def __init__(self)
+self.surname = None
+self.nale = None
+self.family = None
+def getName(self):
+return self.name 
+def setName(self,name):
+self.name = name
+self.StudentModel = StudentModel()
+nameLE = QLineEdit()
+def setName(self,text):
+self.StudentModel.name = text
